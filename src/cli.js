0f41c1aaa2b6bc8dbf530eac781f44102c02d242
William Belle <william.belle@gmail.com>
2021-04-09 17:16:17 +0200

    update copyright date

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /*
- * (c) William Belle, 2019-2020.
+ * (c) William Belle, 2019-2021.
  * See the LICENSE file for more details.
  */
 
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 /*
- * (c) William Belle, 2019-2020.
+ * (c) William Belle, 2019-2021.
  * See the LICENSE file for more details.
  */
 
